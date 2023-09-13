a64403a04c7b4c4037ac41019ab21fe258fa97d7
Sergi Fuster Dura <61521158+SergiFuster@users.noreply.github.com>
2023-09-13 12:30:41 +0200

    Ejercicios del 1 al 4 de la practica 1

--- Lab 1/p1.py
+++ Lab 1/p1.py
@@ -9,8 +9,8 @@ import visualPercepUtils as vpu
 
 def histeq(im, nbins=256):
     imhist, bins = np.histogram(im.flatten(), list(range(nbins)), density=False)
-    cdf = imhist.cumsum() # cumulative distribution function (CDF) = cummulative histogram
-    factor = 255 / cdf[-1]  # cdf[-1] = last element of the cummulative sum = total number of pixels)
+    cdf = imhist.cumsum()  # cumulative distribution function (CDF) = cumulative histogram
+    factor = 255 / cdf[-1]  # cdf[-1] = last element of the cumulative sum = total number of pixels)
     im2 = np.interp(im.flatten(), bins[:-1], factor*cdf)
     return im2.reshape(im.shape), cdf
 
@@ -21,12 +21,28 @@ def testHistEq(im):
 def darkenImg(im,p=2):
     return (im ** float(p)) / (255 ** (p - 1)) # try without the float conversion and see what happens
 
-def brightenImg(im,p=2):
+
+def brightenImg(im, p=2):
     return np.power(255.0 ** (p - 1) * im, 1. / p)  # notice this NumPy function is different to the scalar math.pow(a,b)
 
 
+def invertImg(im):
+    return 255 - im
+
+
+def checkBoardImg(im, m, n):
+    m_pixels = im.shape[0] // m
+    n_pixels = im.shape[1] // n
+    for fil in range(m):
+        for col in range(n):
+            if fil % 2 == col % 2:
+                im[fil*m_pixels:(fil+1)*m_pixels, col*n_pixels:(col+1)*n_pixels] = \
+                    (invertImg(im[fil*m_pixels:(fil+1)*m_pixels, col*n_pixels:(col+1)*n_pixels]))
+    return im
+
+
 def testDarkenImg(im):
-    im2 = darkenImg(im,p=2) #  Is "p=2" different here than in the function definition? Can we remove "p=" here?
+    im2 = darkenImg(im, p=2)  # Is "p=2" different here than in the function definition? Can we remove "p=" here?
     return [im2]
 
 
@@ -34,6 +50,13 @@ def testBrightenImg(im):
     p=2
     im2=brightenImg(im,p)
     return [im2]
+
+
+def testCheckBoardImg(im):
+    m, n = 8, 8
+    im2 = checkBoardImg(im, m, n)
+    return [im2]
+
 
 path_input = './imgs-P1/'
 path_output = './imgs-out-P1/'
@@ -45,17 +68,28 @@ else:
 
 bAllTests = True
 if bAllTests:
-    tests = ['testHistEq', 'testBrightenImg', 'testDarkenImg']
+    tests = ['testHistEq', 'testBrightenImg', 'testDarkenImg', 'testCheckBoardImg']
 else:
-    tests = ['testHistEq']#['testBrightenImg']
+    tests = ['testHistEq']  # ['testBrightenImg']
+
 nameTests = {'testHistEq': "Histogram equalization",
              'testBrightenImg': 'Brighten image',
-             'testDarkenImg': 'Darken image'}
+             'testDarkenImg': 'Darken image',
+             'testCheckBoardImg': 'Checkerboard Image'}
 suffixFiles = {'testHistEq': '_heq',
                'testBrightenImg': '_br',
-               'testDarkenImg': '_dk'}
+               'testDarkenImg': '_dk',
+               'testCheckBoardImg': '_cb'}
 
 bSaveResultImgs = True
+
+
+def saveImg(imfile, suffix, im2):
+    dirname, basename = os.path.dirname(imfile), os.path.basename(imfile)
+    fname, fext = os.path.splitext(basename)
+    pil_im = Image.fromarray(im2.astype(np.uint8))  # from array to Image
+    pil_im.save(path_output + '//' + fname + suffix + fext)
+
 
 def doTests():
     print("Testing on", files)
@@ -66,14 +100,23 @@ def doTests():
             im2 = out[0]
             vpu.showImgsPlusHists(im, im2, title=nameTests[test])
             if len(out) > 1:
-                vpu.showPlusInfo(out[1],"cumulative histogram" if test=="testHistEq" else None)
+                vpu.showPlusInfo(out[1], "cumulative histogram" if test=="testHistEq" else None)
             if bSaveResultImgs:
-                dirname,basename = os.path.dirname(imfile), os.path.basename(imfile)
-                fname, fext = os.path.splitext(basename)
-                #print(dname,basename)
-                pil_im = Image.fromarray(im2.astype(np.uint8))  # from array to Image
-                pil_im.save(path_output+'//'+fname + suffixFiles[test] + fext)
+                saveImg(imfile, suffixFiles[test], im2)
 
-if __name__== "__main__":
+
+def debug():
+    i = 0
+    for imfile in files:
+        im = np.array(Image.open(imfile).convert('L'))
+        checkboard = checkBoardImg(im, 5, 2)
+        vpu.showImgsPlusHists(im, checkboard, "INVERTED")
+        i += 1
+
+
+if __name__ == "__main__":
+    # debug()
     doTests()
+
+
 
